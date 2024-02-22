913f5258260515798b82727853f5aea5d79af10c
Victor Olaitan <vividvictor@gmail.com>
2024-02-22 16:39:53 +0100

    Test case for updating cart items and checking out

--- cypress/e2e/order.cy.js
+++ cypress/e2e/order.cy.js
@@ -10,13 +10,14 @@ describe('Order a product', () => {
 		cy.get('#pa_color').should('have.length.greaterThan', 0).select(1);
 		cy.get('#pa_size').should('have.length.greaterThan', 0).select(1);
 		cy.get('.single_add_to_cart_button').click();
+		cy.wait(2000);
+		cy.get('.cart-count').should('not.have.value', 0);
 		cy.get('.wc-block-components-notice-banner').should('be.visible');
 		cy.get('.wc-block-components-notice-banner__content > .button')
 			.should('have.attr', 'href', 'https://shop.demoqa.com/cart/')
 			.click();
 		cy.get('.cart_item').should('have.length.greaterThan', 0);
 	});
-
 	it('clears cart', () => {
 		cy.get('.post-1497 > .noo-product-inner').click();
 		cy.get('#pa_color').should('have.length.greaterThan', 0).select(1);
@@ -33,5 +34,57 @@ describe('Order a product', () => {
 			'contain.text',
 			'Your cart is currently empty'
 		);
+	});
+
+	it('updates cart items and checkout', () => {
+		cy.get('.post-1497 > .noo-product-inner').click();
+		cy.get('#pa_color').should('have.length.greaterThan', 0).select(1);
+		cy.get('#pa_size').should('have.length.greaterThan', 0).select(1);
+		cy.get('.single_add_to_cart_button').click();
+		cy.wait(2000);
+		cy.get('.cart-count').should('not.have.value', 0);
+		cy.get('.wc-block-components-notice-banner').should('be.visible');
+		cy.get('.wc-block-components-notice-banner__content > .button')
+			.should('have.attr', 'href', 'https://shop.demoqa.com/cart/')
+			.click();
+		cy.get('.cart_item').should('have.length.greaterThan', 0);
+		cy.get('.cart_item > .product-quantity button[class=qty-increase]')
+			.click()
+			.wait(2000)
+			.then(() => {
+				cy.get(
+					'.cart_item > .product-quantity input[class*=input-text]'
+				).should('have.value', 2);
+			})
+			.wait(2000);
+		cy.get('.actions input[name=update_cart]').click();
+		cy.get('.wc-block-components-notice-banner').should('be.visible');
+		cy.get('.wc-block-components-notice-banner__content').should(
+			'contain.text',
+			'Cart updated.'
+		);
+		cy.get('.continue')
+			.click()
+			.wait(2000)
+			.then(() => {
+				cy.url().should('include', '/shop');
+				cy.get('.products').should('have.length.greaterThan', 0);
+				cy.get('.products > .noo-product-item').first().click();
+				cy.wait(2000);
+
+				cy.get('#pa_color').should('have.length.greaterThan', 0).select(2);
+				cy.get('#pa_size').should('have.length.greaterThan', 0).select(2);
+				cy.get('.single_add_to_cart_button').click();
+
+				cy.get('.cart-button')
+					.should('have.attr', 'href', 'https://shop.demoqa.com/cart/')
+					.click();
+			});
+		cy.get('tbody').should('have.length.greaterThan', 1);
+		// checkouts
+		cy.get('.wc-proceed-to-checkout > .checkout-button')
+			.should('have.attr', 'href', 'https://shop.demoqa.com/checkout/')
+			.click();
+		cy.url().location();
 	});
 });
